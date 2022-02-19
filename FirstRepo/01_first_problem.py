c07cc67118e8dcfca7b8e79436f933cd5f4d81e2
Wardah Rasheed <bp1650251wardahrasheed@gmail.com>
2022-02-19 23:11:27 +0500

    My first Repo

---
+++ FirstRepo/01_first_problem.py
@@ -0,0 +1,8 @@
+print(2+3)
+print("Hello World")
+print("hi I am pathan")
+print("Home Sweet Home")
+print("hello to the new world")
+print("I am going to  become a wealthy person")
+print("I have to do this")
+print("I will complete learn python")
